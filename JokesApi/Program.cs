156ad40b7b71b0d9507431ac25e58d5ced264a6a
201903 <201903@1215-02>
2021-06-19 14:16:14 +0300

    слили бота и апи

--- JokesApi/Program.cs
+++ JokesApi/Program.cs
@@ -6,79 +6,11 @@ using System.IO;
 using System.Net;
 using Newtonsoft.Json;
 using System.Collections.Generic;
+using Telegram.Bot;
 
 namespace Api_Joke
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-           
-            var category = "knock-knock";
-            // other categories: job, animal, blonde, 
-            var lang = "en";
-            var url = $"https://api.jokes.one/jod?category={category}&language={lang}&";
-
-            var request = WebRequest.Create(url);
-
-            var response = request.GetResponse();
-            var httpStatusCode = (response as HttpWebResponse).StatusCode;
-
-            if (httpStatusCode != HttpStatusCode.OK)
-            {
-                Console.WriteLine(httpStatusCode);
-                return;
-            }
-
-            using (var streamReader = new StreamReader(response.GetResponseStream()))
-            {
-                string result = streamReader.ReadToEnd();
-                //Console.WriteLine(result);
-                var Jokes = JsonConvert.DeserializeObject<Root>(result);
-
-                Console.WriteLine(Jokes.contents.jokes[0].joke.text);
-            }
-
-        }
-    }
-
-    public class Success
-    {
-        public int total { get; set; }
-    }
-
-    public class Joke2
-    {
-        public string title { get; set; }
-        public string lang { get; set; }
-        public string length { get; set; }
-        public string clean { get; set; }
-        public string racial { get; set; }
-        public string id { get; set; }
-        public string text { get; set; }
-    }
-
-    public class Joke
-    {
-        public string description { get; set; }
-        public string language { get; set; }
-        public string background { get; set; }
-        public string category { get; set; }
-        public string date { get; set; }
-        public Joke2 joke { get; set; }
-    }
-
-    public class Contents
-    {
-        public List<Joke> jokes { get; set; }
-        public string copyright { get; set; }
-    }
-
-    public class Root
-    {
-        public Success success { get; set; }
-        public Contents contents { get; set; }
-    }
+  
 
 
 }
--- Tg.bot/Program.cs
+++ Tg.bot/Program.cs
@@ -5,25 +5,96 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using Telegram.Bot;
+using System.Net;
+using Newtonsoft.Json;
 
-    namespace Tg.bot
+namespace Tg.bot
     {
     class Program
     {
         static void Main(string[] args)
         {
 
+            var category = "jod";
+            // other categories: jod, animal, blonde
+
+            var lang = "en";
+            string res = "";
+
             TelegramBotClient bot = new TelegramBotClient("1884107228:AAHlM-81LFKVMKIFNbiuIjOJ9Qdalfg8x_s");
 
             bot.OnMessage += (s, arg) =>
             {
+
                 Console.WriteLine($"{arg.Message.Chat.FirstName}: {arg.Message.Text}");
+                var url = $"https://api.jokes.one/jod?category={category}&language={lang}&";
+                
                 bot.SendTextMessageAsync(arg.Message.Chat.Id, $"Ты говоришь: {arg.Message.Text}");
+
+                var request = WebRequest.Create(url);
+
+                var response = request.GetResponse();
+                var httpStatusCode = (response as HttpWebResponse).StatusCode;
+
+                if (httpStatusCode != HttpStatusCode.OK)
+                {
+                    Console.WriteLine(httpStatusCode);
+                    return;
+                }
+
+                using (var streamReader = new StreamReader(response.GetResponseStream()))
+                {
+                    string result = streamReader.ReadToEnd();
+                    //Console.WriteLine(result);
+                    var Jokes = JsonConvert.DeserializeObject<Root>(result);
+
+                    res += Jokes.contents.jokes[0].joke.text;
+
+                }
+                bot.SendTextMessageAsync(arg.Message.Chat.Id, $"Joke: {res}");
+
             };
 
             bot.StartReceiving();
 
             Console.ReadKey();
         }
+    }
+    public class Success
+    {
+        public int total { get; set; }
+    }
+
+    public class Joke2
+    {
+        public string title { get; set; }
+        public string lang { get; set; }
+        public string length { get; set; }
+        public string clean { get; set; }
+        public string racial { get; set; }
+        public string id { get; set; }
+        public string text { get; set; }
+    }
+
+    public class Joke
+    {
+        public string description { get; set; }
+        public string language { get; set; }
+        public string background { get; set; }
+        public string category { get; set; }
+        public string date { get; set; }
+        public Joke2 joke { get; set; }
+    }
+
+    public class Contents
+    {
+        public List<Joke> jokes { get; set; }
+        public string copyright { get; set; }
+    }
+
+    public class Root
+    {
+        public Success success { get; set; }
+        public Contents contents { get; set; }
     }
 }

2721757bbe9e275819305eb931d5edb8657077f3
201903 <201903@1215-02>
2021-06-19 13:44:50 +0300

    Сделал эхо бота

---
+++ Tg.bot/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Telegram.Bot;
+
+    namespace Tg.bot
+    {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TelegramBotClient bot = new TelegramBotClient("1884107228:AAHlM-81LFKVMKIFNbiuIjOJ9Qdalfg8x_s");
+
+            bot.OnMessage += (s, arg) =>
+            {
+                Console.WriteLine($"{arg.Message.Chat.FirstName}: {arg.Message.Text}");
+                bot.SendTextMessageAsync(arg.Message.Chat.Id, $"You say: {arg.Message.Text}");
+            };
+
+            bot.StartReceiving();
+
+            Console.ReadKey();
+        }
+    }
+}
